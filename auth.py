6b2a86acd174ef478a766c7e423d85cae086c988
TonyChol <tonycai1992@gmail.com>
2015-11-25 22:25:47 -0500

    Finish: authenticate

---
+++ auth.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+'''
+    This is the authenticate module for imgur using ImgurClient.
+'''
+import base64
+from imgurpython import ImgurClient
+from helpers import get_input, get_config
+
+def authenticate():
+    """
+    :rtype: client object
+    """
+    config = get_config()
+    config.read('auth.ini')
+    client_id = config.get('credentials','client_id')
+    client_secret = config.get('credentials', 'client_secret')
+    refresh_token = config.get('credentials', 'refresh_token')
+    access_token = config.get('credentials', 'access_token')
+
+    client = ImgurClient(client_id, client_secret)
+    if not refresh_token:
+        print 'not refresh token'
+        authorization_url = client.get_auth_url('pin')
+        print("Go to the following URL: {0}".format(authorization_url))
+
+        pin = get_input("Enter the pin code: ")
+
+        # ... redirect user to `authorization_url`, obtain pin (or code or token) ...
+        credentials = client.authorize(pin, 'pin')
+
+        # Store the refresh_token
+        new_refresh_token = credentials['refresh_token']
+        config.set('credentials', 'refresh_token', value=new_refresh_token)
+        refresh_token = new_refresh_token
+        with open(r'auth.ini', 'wb') as configfile:
+            config.write(configfile)
+
+    if refresh_token and not access_token:
+        print 'refresh token but not access token'
+        print 'refresh token: {0}'.format(refresh_token)
+        client.set_user_auth(access_token, refresh_token)
+        # client = ImgurClient(client_id, client_secret, refresh_token)
+        if not client.auth:
+            print 'no auth!!!'
+        if client.auth:
+            print "there is a client.auth"
+            client.auth.refresh()
+            new_access_token = client.auth.current_access_token
+            print 'new access token: {0}'.format(new_access_token)
+            config.set('credentials', 'access_token', value=new_access_token)
+            print 'writing access token'
+            with open(r'auth.ini', 'wb') as configfile:
+                config.write(configfile)
+            access_token = new_access_token
+
+    if refresh_token and access_token:
+        print 'refresh token and access token'
+        client = ImgurClient(client_id, client_secret, access_token, refresh_token)
+
+    return client
+
+
+# method_list = [method for method in dir(client) if callable(getattr(client, method))]
+
+if __name__ == '__main__':
+    client = authenticate()
+    print client
+    images = client.get_account_images('me')
+    print "Current # of images: {0}".format(len(images))
+
+    # path = '~/Dropbox/5.Personal/Photos/avatars/cityU.jpg'
+    # fd = open(path, 'rb')
+    # contents = fd.read()
+    # b64 = base64.b64encode(contents)
+    # print type(b64)
